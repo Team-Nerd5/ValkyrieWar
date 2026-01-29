7b1b661a08f7e619f84bfd92f4a6b75f2028aca5
hyunwoo <allegoria88@gmail.com>
2026-01-29 15:51:40 +0900

    [성현우] 이벤트 시스템 호출 라이브러리 세팅

---
+++ Source/ValkyrieWar/Private/GameSystem/Library/GameBaseLibrary.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSystem/Library/GameBaseLibrary.h"
+
+UWorldEventSystem* UGameBaseLibrary::GetWorldEventSystem(const UObject* WorldContextObject)
+{
+	if (!WorldContextObject)
+		return nullptr;
+
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	if (!World)
+		return nullptr;
+
+	return World->GetSubsystem<UWorldEventSystem>();
+}
---
+++ Source/ValkyrieWar/Public/GameSystem/Library/GameBaseLibrary.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameBaseLibrary.generated.h"
+
+class UWorldEventSystem;
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UGameBaseLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "MyGame|Utils", meta = (WorldContext = "WorldContextObject"))
+	static UWorldEventSystem* GetWorldEventSystem(const UObject* WorldContextObject);
+};
