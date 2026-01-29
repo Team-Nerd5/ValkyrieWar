a1b047b58eee637ab26b61252d494b52a13cee08
hyunwoo <allegoria88@gmail.com>
2026-01-29 15:36:47 +0900

    [성현우] 이벤트 호출 구조 세팅
    - 기본 예시로 위젯 이벤트 구조체만 생성

---
+++ Source/ValkyrieWar/Public/Data/Event/WidgetEventData.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetEventData.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct VALKYRIEWAR_API FWidgetEventData
+{
+	GENERATED_BODY()
+
+	//위젯 이벤트 Delegate 모음
+};
---
+++ Source/ValkyrieWar/Public/GameSystem/Instance/World/WorldEventSystem.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "Data/Event/WidgetEventData.h"
+#include "WorldEventSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UWorldEventSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+	FWidgetEventData Widget;
+};
