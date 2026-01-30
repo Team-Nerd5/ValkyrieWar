9f0c61cb8860a7c8d9a4bf64040122a7442948c7
hyunwoo <allegoria88@gmail.com>
2026-01-30 12:05:28 +0900

    [성현우] UI 호출 Enum으로 호출하는 형태로 수정
    - UI Class GameInstance에서 전역관리로 변경
    - enum과 테이블 데이터로 위젯 클래스 관리

---
+++ Source/ValkyrieWar/Public/Data/Enums.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class E_UITYPE : uint8
+{
+	//HUD
+	Login,
+	Lobby,
+	Battle,
+
+	//Popup
+	
+};
