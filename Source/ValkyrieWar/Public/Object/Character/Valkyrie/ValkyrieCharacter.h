0bd43328927d5703bc3943948f627fb86b407f3c
hyunwoo <allegoria88@gmail.com>
2026-01-30 14:52:10 +0900

    [성현우] 기존 탑다운 캐릭터 코드 이전
    - 게임모드, 컨트롤러, 입력 이전

---
+++ Source/ValkyrieWar/Public/Object/Character/Valkyrie/ValkyrieCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseCharacter.h"
+#include "ValkyrieCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API AValkyrieCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
