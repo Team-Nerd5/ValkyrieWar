30b3e61648dce81998cc507574f8d04199711e9f
hyunwoo <allegoria88@gmail.com>
2026-01-30 15:10:38 +0900

    [성현우] 기본코드 복구

---
+++ Source/ValkyrieWar/ValkyrieWar.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ValkyrieWar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ValkyrieWar, "ValkyrieWar" );
+
+DEFINE_LOG_CATEGORY(LogValkyrieWar)
+ 
