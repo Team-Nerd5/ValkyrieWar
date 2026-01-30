8c3468ab217490781f80c8c3893b3f1eaa641dbc
hyunwoo <allegoria88@gmail.com>
2026-01-30 14:52:39 +0900

    [성현우]게임모드 새로 세팅 및 로그인 기본 맵 생성(블랙)

---
+++ Source/ValkyrieWar/Private/GameSystem/GameMode/ValkyrieWarGameMode.cpp
@@ -0,0 +1,8 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameSystem/GameMode/ValkyrieWarGameMode.h"
+
+AValkyrieWarGameMode::AValkyrieWarGameMode()
+{
+
+}
---
+++ Source/ValkyrieWar/Public/GameSystem/GameMode/ValkyrieWarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ValkyrieWarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AValkyrieWarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AValkyrieWarGameMode();
+};
+
+
+
