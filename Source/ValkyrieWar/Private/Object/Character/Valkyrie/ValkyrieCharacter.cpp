41eb49a7bc8cdc5a7c63c2e9e3f16f501a9dcd25
Dumbass <wjdtjdgus4548@gmail.com>
2026-02-04 15:17:23 +0900

    [정성현] 카메라 터치 드래그로 이동

--- Source/ValkyrieWar/Private/Object/Character/Valkyrie/ValkyrieCharacter.cpp
+++ Source/ValkyrieWar/Private/Object/Character/Valkyrie/ValkyrieCharacter.cpp
@@ -1,5 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Object/Character/Valkyrie/ValkyrieCharacter.h"
+
+AValkyrieCharacter::AValkyrieCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AValkyrieCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AValkyrieCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
 
--- Source/ValkyrieWar/Public/Object/Character/Valkyrie/ValkyrieCharacter.h
+++ Source/ValkyrieWar/Public/Object/Character/Valkyrie/ValkyrieCharacter.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -13,5 +13,13 @@ UCLASS()
 class VALKYRIEWAR_API AValkyrieCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	AValkyrieCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
 };
