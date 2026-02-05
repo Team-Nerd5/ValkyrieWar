42e306189769e6390ccfc00d162349408204f744
LEI-WULONG <dngustn3@gmail.com>
2026-02-05 17:15:45 +0900

    [우현수] 세이브로드 기본세팅

---
+++ Source/ValkyrieWar/Public/Data/Save/PlayerAccountData.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerAccountData.generated.h"
+
+USTRUCT(BlueprintType)
+struct VALKYRIEWAR_API FPlayerAccountData
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY()
+    FString PlayerName = TEXT("Player");
+
+    UPROPERTY()
+    int32 PlayerLevel = 1;
+
+    UPROPERTY()
+    int32 PlayerExp = 0;
+
+};
