d6c71b6ce0bfefe3c4d180a764d80d300a228f02
LEI-WULONG <dngustn3@gmail.com>
2026-02-05 17:15:45 +0900

    [우현수] 세이브로드 기본세팅

---
+++ Source/ValkyrieWar/Public/Object/Save/ValkyrieWarSaveGame.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "Data/Save/PlayerAccountData.h"
+#include "ValkyrieWarSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UValkyrieWarSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	FPlayerAccountData PlayerAccountData;
+
+};
