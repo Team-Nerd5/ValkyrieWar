22aae081be1d9114afa72f0f38a925678f615d98
LEI-WULONG <dngustn3@gmail.com>
2026-02-05 17:15:45 +0900

    [우현수] 세이브로드 기본세팅

---
+++ Source/ValkyrieWar/Private/GameSystem/Instance/Save/SaveManager.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSystem/Instance/Save/SaveManager.h"
+
+#include "Object/Save/ValkyrieWarSaveGame.h"
+
+#include "Kismet/GameplayStatics.h"
+
+void USaveManager::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	LoadGame();
+}
+
+void USaveManager::Deinitialize()
+{
+	SaveGame();
+	CachedSaveGame = nullptr;
+
+	Super::Deinitialize();
+}
+
+void USaveManager::SetPlayerAccountData(const FPlayerAccountData& InPlayerAccountData)
+{
+	CurrentPlayerAccountData = InPlayerAccountData;
+
+	SaveGame();
+}
+
+void USaveManager::SaveGame()
+{
+	if (CachedSaveGame)
+	{
+		CachedSaveGame->PlayerAccountData = CurrentPlayerAccountData;
+
+		bool bIsSuccess = UGameplayStatics::SaveGameToSlot(CachedSaveGame, SaveSlotName, SaveIndex);
+		if (bIsSuccess)
+		{
+			UE_LOG(LogTemp, Log, TEXT("저장 성공"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Log, TEXT("저장 실패"));
+		}
+	}
+
+}
+
+void USaveManager::LoadGame()
+{
+	if (UGameplayStatics::DoesSaveGameExist(SaveSlotName, SaveIndex))
+	{
+		UValkyrieWarSaveGame* LoadedSaveGame = Cast<UValkyrieWarSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlotName, SaveIndex));
+
+		if (LoadedSaveGame)
+		{
+			CurrentPlayerAccountData = LoadedSaveGame->PlayerAccountData;
+			CachedSaveGame->PlayerAccountData = CurrentPlayerAccountData;
+			UE_LOG(LogTemp, Log, TEXT("저장파일 로드 성공"));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Log, TEXT("저장파일 로드 실패"));
+		}
+	}
+
+}
---
+++ Source/ValkyrieWar/Public/GameSystem/Instance/Save/SaveManager.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Data/Save/PlayerAccountData.h"
+#include "SaveManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API USaveManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+	virtual void Deinitialize() override;
+
+public: // 게터 / 세터
+	UFUNCTION(BlueprintCallable, Category = "SaveSystem")
+	inline FPlayerAccountData GetPlayerAccountData() const { return CurrentPlayerAccountData; }
+
+	UFUNCTION(BlueprintCallable, Category = "SaveSystem")
+	void SetPlayerAccountData(const FPlayerAccountData& InPlayerData);
+
+public: // 저장 / 로드
+	UFUNCTION(BlueprintCallable, Category = "SaveSystem")
+	void SaveGame();
+
+	UFUNCTION(BlueprintCallable, Category = "SaveSystem")
+	void LoadGame();
+
+protected:
+	UPROPERTY()
+	FString SaveSlotName = TEXT("PlayerSave01");
+
+	UPROPERTY()
+	int32 SaveIndex = 0;
+
+	UPROPERTY()
+	TObjectPtr<class UValkyrieWarSaveGame> CachedSaveGame = nullptr;
+
+	FPlayerAccountData CurrentPlayerAccountData;
+};
