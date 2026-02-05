23c62813ddd3d6c9b41f53afdf2aca4db97a72f2
Dumbass <wjdtjdgus4548@gmail.com>
2026-02-05 17:38:19 +0900

    [정성현] 이동시 카메라 부착기능 구현 완료

--- Source/ValkyrieWar/Private/GameSystem/Base/BaseCharacter.cpp
+++ Source/ValkyrieWar/Private/GameSystem/Base/BaseCharacter.cpp
@@ -32,19 +32,11 @@ ABaseCharacter::ABaseCharacter()
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
-	// Create a camera boom...
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
-	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
-	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
-
 	// Create a camera...
 	TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-	TopDownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
+	TopDownCameraComponent->SetupAttachment(RootComponent);
+	
+	TopDownCameraComponent->SetWorldRotation(FRotator(-45.0f, 0.0f, 0.0f));
 	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
 
 	// Activate ticking in order to update the cursor every frame.
