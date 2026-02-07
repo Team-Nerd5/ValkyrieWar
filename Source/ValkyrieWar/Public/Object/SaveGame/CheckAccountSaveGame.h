c9d082b29f5e66c30eff88191f1d20301b419bba
edwardsoung <allegoria88@Gmail.com>
2026-02-08 02:19:23 +0900

    [성현우] 데이터 저장/로드 수정
    - 암호화 처리 추가
    - 암호화 헬퍼로 빼둠

---
+++ Source/ValkyrieWar/Public/Object/SaveGame/CheckAccountSaveGame.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "CheckAccountSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UCheckAccountSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY()
+	uint64 UserId = 0;
+};
