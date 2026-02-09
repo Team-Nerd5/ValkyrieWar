705cd70f18817db00b01b0ac0a9bac090aedffae
sdchjjj <sdchjjj@gmail.com>
2026-02-09 16:20:51 +0900

    [서동철] 유닛 기본 요소 구현
    # Conflicts:
    #	Source/ValkyrieWar/Public/Data/Enums.h
    #	ValkyrieWar.uproject

---
+++ Source/ValkyrieWar/Public/Test/SDCH/TestUnit/TestAI/BTTask_TestAttackReservedTarget.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_TestAttackReservedTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UBTTask_TestAttackReservedTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+    UBTTask_TestAttackReservedTarget();
+
+    UPROPERTY(EditAnywhere, Category = "BB")
+    struct FBlackboardKeySelector ReservedTargetKey;
+
+    UPROPERTY(VisibleAnywhere, Category = "BB")
+    struct FBlackboardKeySelector AttackTryCountKey;
+
+protected:
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
---
+++ Source/ValkyrieWar/Public/Test/SDCH/TestUnit/TestAI/BTTask_TestMoveToReservedTarget.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_TestMoveToReservedTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UBTTask_TestMoveToReservedTarget : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+    UBTTask_TestMoveToReservedTarget();
+
+    UPROPERTY(EditAnywhere, Category = "BB")
+    struct FBlackboardKeySelector ReservedTargetKey;
+
+    UPROPERTY(EditAnywhere, Category = "BB")
+    struct FBlackboardKeySelector EnemyBaseKey;
+
+    UPROPERTY(EditAnywhere)
+    float AcceptableRadius = 80.f;
+
+    UPROPERTY(EditAnywhere)
+    bool bUseUnitAttackRangeAsRadius = true;
+
+    UPROPERTY(EditAnywhere)
+    float RadiusScale = 0.9f;
+
+protected:
+    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
