2bf0126b83d5a4bf89caa2a60004ba9ecdc2de3c
sdchjjj <sdchjjj@gmail.com>
2026-02-09 16:20:51 +0900

    [서동철] 유닛 기본 요소 구현
    # Conflicts:
    #	Source/ValkyrieWar/Public/Data/Enums.h
    #	ValkyrieWar.uproject

---
+++ Source/ValkyrieWar/Private/Test/SDCH/TestUnit/TestComponents/TestUnitBrainComponent.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test/SDCH/TestUnit/TestComponents/TestUnitBrainComponent.h"
+#include "Test/SDCH/TestUnit/TestCharacters/TestBaseUnit.h"
+
+UTestUnitBrainComponent::UTestUnitBrainComponent()
+{
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTestUnitBrainComponent::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void UTestUnitBrainComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    // 서브시스템이 정리하는 게 원칙이지만, 안전장치로 여기서도 비움
+    ReservedTarget.Reset();
+    Super::EndPlay(EndPlayReason);
+}
+
+bool UTestUnitBrainComponent::CanChangeReservation(float Now) const
+{
+    return (Now - LastReservationChangeTime) >= MinHoldSeconds;
+}
+
+void UTestUnitBrainComponent::SetReservedTarget(AActor* NewTarget, float Now)
+{
+    ReservedTarget = NewTarget;
+    LastReservationChangeTime = Now;
+    ReservationState = (NewTarget ? EReservationState::Reserved : EReservationState::None);
+}
+
+void UTestUnitBrainComponent::ClearReservedTarget(float Now)
+{
+    ReservedTarget.Reset();
+    LastReservationChangeTime = Now;
+    ReservationState = EReservationState::None;
+}
+
+ATestBaseUnit* UTestUnitBrainComponent::GetOwnerUnit() const
+{
+    return Cast<ATestBaseUnit>(GetOwner());
+}
---
+++ Source/ValkyrieWar/Public/Test/SDCH/TestUnit/TestComponents/TestUnitBrainComponent.h
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Data/Enums.h"
+#include "TestUnitBrainComponent.generated.h"
+
+class ATestBaseUnit;
+
+USTRUCT(BlueprintType)
+struct FTestEngagementSlot
+{
+    GENERATED_BODY()
+
+    // 슬롯에 들어온 공격자
+    UPROPERTY()
+    TWeakObjectPtr<AActor> Attacker;
+
+    // 슬롯을 확보한 시간 (thrashing 방지/디버그 용)
+    UPROPERTY()
+    float ReservedAtTime = 0.f;
+
+    bool IsValid() const { return Attacker.IsValid(); }
+    void Reset() { Attacker.Reset(); ReservedAtTime = 0.f; }
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VALKYRIEWAR_API UTestUnitBrainComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+    UTestUnitBrainComponent();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Team")
+    ETeam Team = ETeam::TeamA;
+
+    // 타깃 선택 룰셋(서브시스템은 슬롯/attacker 제한을 유지하고,
+    // 여기서는 "어떤 후보를 우선할지"만 결정)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Targeting")
+    ETargetingPolicy TargetingPolicy = ETargetingPolicy::Auto;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    EReservationState ReservationState = EReservationState::None;
+
+    // 내가 공격하기로 예약한 대상
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    TWeakObjectPtr<AActor> ReservedTarget;
+
+    // 내가 목표로 삼는 '적 기지' (기본: 적 성벽 WallAnchor 중 1개)
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+    TWeakObjectPtr<AActor> EnemyBase;
+
+    // 타깃을 얼마나 자주 바꿀지(Thrashing 방지)
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Tuning")
+    float MinHoldSeconds = 1.f;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Runtime")
+    float LastReservationChangeTime = -1000.f;
+
+public:
+    bool CanChangeReservation(float Now) const;
+    void SetReservedTarget(AActor* NewTarget, float Now);
+    void ClearReservedTarget(float Now);
+
+    UFUNCTION(BlueprintCallable)
+    AActor* GetReservedTarget() const { return ReservedTarget.Get(); }
+
+    UFUNCTION(BlueprintCallable)
+    ATestBaseUnit* GetOwnerUnit() const;
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+};
