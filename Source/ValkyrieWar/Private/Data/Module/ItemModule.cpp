987948ad1a644a02e3007c2684b7da5b8f2c0c87
edwardsoung <allegoria88@Gmail.com>
2026-02-09 23:48:54 +0900

    [성현우] 데이터 처리 구조 작업(우선 아이템 기반)
    - Module / Data / RowDat 구조로 분리
    - BaseModule로 상속구조 분리

---
+++ Source/ValkyrieWar/Private/Data/Module/ItemModule.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Data/Module/ItemModule.h"
+
+void UItemModule::Initialize(UGameManager* InGameManager)
+{
+	Super::Initialize(InGameManager);
+
+	if (GameManager.IsValid())
+	{
+		Table = GameManager->GetGameData(ETableDataType::Item);
+
+		if (Table)
+		{
+			//데이터 변환해서 자료형으로 보관..
+			//여기서 그냥 배열로만 들고있고
+			//인벤토리 따로 구현해서 관리해도 괜찮을 듯..?
+			//여기로 업데이트 하고 저장하는쪽으로 보내는 용도로 사용...
+			// 캐릭터도 비슷하게 사용가능은 할듯
+			// 나머지는 따로 안만들고 가능하긴 한데..			
+		}
+	}
+}
---
+++ Source/ValkyrieWar/Public/GameSystem/Library/Data/ItemDataFactory.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Data/Game/ItemData.h"
+#include "Data/Table/GameData/ItemDataRow.h"
+#include "ItemDataFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UItemDataFactory : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	
+	//id 기반으로 아이템 데이터 생성
+	static UItemData* Make(int32 InDataId, int32 Amount);
+};
