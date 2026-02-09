5e63ca47c54b38b91523b58ecca3324a96cfa471
hyunwoo <allegoria88@gmail.com>
2026-02-09 17:59:04 +0900

    [성현우] TableData용 데이터 구조 추가
    # Conflicts:
    #	Source/ValkyrieWar/Public/Data/Enums.h

--- Source/ValkyrieWar/Private/GameSystem/Instance/Game/DataManager.cpp
+++ Source/ValkyrieWar/Private/GameSystem/Instance/Game/DataManager.cpp
@@ -1,4 +1,9 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
+<<<<<<<< HEAD:Source/ValkyrieWar/Private/GameSystem/Instance/Game/DataManager.cpp
 #include "GameSystem/Instance/Game/DataManager.h"
+========
+#include "Data/Game/ItemData.h"
+
+>>>>>>>> b8b970d ([성현우] TableData용 데이터 구조 추가):Source/ValkyrieWar/Private/Data/Game/ItemData.cpp
