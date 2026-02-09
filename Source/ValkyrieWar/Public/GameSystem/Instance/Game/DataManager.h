bb221689f35019d761163c0102257aff5d5b251e
edwardsoung <allegoria88@Gmail.com>
2026-02-09 23:48:54 +0900

    [성현우] 데이터 처리 구조 작업(우선 아이템 기반)
    - Module / Data / RowDat 구조로 분리
    - BaseModule로 상속구조 분리

---
+++ Source/ValkyrieWar/Public/GameSystem/Instance/Game/DataManager.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Data/Module/ItemModule.h"
+#include "DataManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UDataManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	//void InitItem(/*Data*/);
+
+public:
+	FORCEINLINE UItemModule* GetItemModule() const { return ItemModule; }
+private:
+	UPROPERTY()
+	TObjectPtr<UItemModule> ItemModule = nullptr;
+};
