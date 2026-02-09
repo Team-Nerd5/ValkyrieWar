b8b970d060ecbc5783e55b546e68d4cb87e09d6f
hyunwoo <allegoria88@gmail.com>
2026-02-09 17:59:04 +0900

    [성현우] TableData용 데이터 구조 추가
    # Conflicts:
    #	Source/ValkyrieWar/Public/Data/Enums.h

---
+++ Source/ValkyrieWar/Public/Data/Table/GameData/ContentsDataRow.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/Enums.h"
+#include "ContentsDataRow.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct VALKYRIEWAR_API FContentsDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere)
+	int32 DataId;
+	UPROPERTY(EditAnywhere)
+	FString Name;
+	UPROPERTY(EditAnywhere)
+	TSoftObjectPtr<UTexture2D> Icon = nullptr;
+	UPROPERTY(EditAnywhere)
+	EUIType OpenPopup;
+	UPROPERTY(EditAnywhere)
+	int32 Order;
+
+};
---
+++ Source/ValkyrieWar/Public/Data/Table/GameData/RewardDataRow.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/Enums.h"
+#include "RewardDataRow.generated.h"
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct VALKYRIEWAR_API FRewardDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere)
+	int32 GroupId;
+	UPROPERTY(EditAnywhere)
+	EItemType ItemType;
+	UPROPERTY(EditAnywhere)
+	int32 DataId;
+	UPROPERTY(EditAnywhere)
+	int32 Amount = 0;
+};
