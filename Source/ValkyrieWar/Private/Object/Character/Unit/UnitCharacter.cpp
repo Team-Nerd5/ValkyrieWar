f2c9a5654dfaa73bfff132ede93189cc6ddd31ca
hyunwoo <allegoria88@gmail.com>
2026-02-11 18:28:28 +0900

    [성현우] 유닛 캐릭터 코드 세팅
    - 파츠 부모 세팅 기본으로 해주는 코드 추가

---
+++ Source/ValkyrieWar/Private/Object/Character/Unit/UnitCharacter.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/Character/Unit/UnitCharacter.h"
+
+void AUnitCharacter::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    //하위 파츠 세팅
+    USkeletalMeshComponent* LeaderMesh = GetMesh();
+
+    TArray<USkeletalMeshComponent*> ModularParts;
+    GetComponents<USkeletalMeshComponent>(ModularParts);
+
+    for (USkeletalMeshComponent* Part : ModularParts)
+    {
+        if (Part == LeaderMesh) continue;
+
+        Part->SetLeaderPoseComponent(LeaderMesh);
+    }
+}
---
+++ Source/ValkyrieWar/Public/Object/Character/Unit/UnitCharacter.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseCharacter.h"
+#include "UnitCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API AUnitCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PostInitializeComponents() override;
+};
