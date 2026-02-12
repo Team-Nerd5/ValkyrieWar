5162c802f7a1e6840ed6b12ad1f61223a1058651
hyunwoo <allegoria88@gmail.com>
2026-02-12 10:43:52 +0900

    [성현우] Enum 정리 및 파일 분할

---
+++ Source/ValkyrieWar/Public/Data/Enum/DataEnums.h
@@ -0,0 +1,98 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EGoodsType : uint8
+{
+	Ticket,
+	Gem,
+	Gold,
+};
+
+UENUM(BlueprintType)
+enum class EItemType : uint8
+{
+	None,
+	Goods,
+	Equip,
+	Item,
+};
+
+//아이템 필터용 그룹
+UENUM(BlueprintType)
+enum class EItemGroup : uint8
+{
+	None,
+	Weapon,
+	Armor,
+	Helmet,
+	Growth,
+};
+
+UENUM(BlueprintType)
+enum class ESkillType : uint8
+{
+	None,
+	Damage,
+	Heal,
+	Splash,
+};
+
+UENUM(BlueprintType)
+enum class EAttributeType : uint8
+{
+	Health,
+	Defence,
+	Attack,
+	MoveSpeed,
+};
+
+UENUM(BlueprintType)
+enum class ESkillApplyType : uint8
+{
+	Instant,
+	PerSec,
+};
+
+UENUM(BlueprintType)
+enum class ETargetType : uint8
+{
+	InRange,
+	LowHpInRange,
+	HighHpInRange,
+	Camp,
+};
+
+UENUM(BlueprintType)
+enum class ETableDataType : uint8
+{
+	Valkyrie,
+	Attack,
+	Contents,
+	Goods,
+	Item,
+	Reward,
+	Skill,
+	SkillEffect,
+	Stage,
+	StageInfo,
+	StageReward,
+	StatGroup,
+	Unit,
+};
+
+UENUM(BlueprintType)
+enum class ESaveType : uint8
+{
+	CheckAccount,
+	Account,
+	Gacha,
+	Goods,
+	Item,
+	Stage,
+	UnitUpgrade,
+	Valkyrie
+};
---
+++ Source/ValkyrieWar/Public/Data/Enum/StateEnums.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class ELoginState : uint8
+{
+	Init,
+	LoadData,
+	CheckAccount,
+	CreateAccount,
+	MakeSaveData,
+	MoveToLobby,
+};
+
+
+UENUM(BlueprintType)
+enum class EBattleState : uint8
+{
+	Init,		//최초 상태. 데이터 로드 시작
+	Ready,		//데이터 및 필요 오브젝트 로드 완료
+	Start,		//게임 시작 연출 발키리 뭐 동작 정도?
+	Play,		//전투 중
+	Win,		//전투 승리로 종료
+	Defeat,		//전투 패배로 종료
+	TimeOver,	//시간초과로 종료(패배)
+	MoveToLobby,//로비로 이동
+};
+
+UENUM(BlueprintType)
+enum class ELobbyState : uint8
+{
+	Init,			//로비 데이터 호출 시작
+	Ready,			//인벤토리 등 데이터 로드 완료, UI 호출
+	MoveToStage,	//스테이지로 레벨 전환
+};
