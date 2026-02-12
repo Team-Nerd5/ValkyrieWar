8721057e08fcb55f2b00445e7ec173362dfd0ed9
BestSellerrr <rhkdals0921@daum.net>
2026-02-12 18:00:15 +0900

    [오광민] 테스트용 인벤토리 위젯 생성 및 필요 함수 추가
    에디터 테스트 폴더 정리, 인벤토리 위젯 함수 내용 추가 필요

---
+++ Source/ValkyrieWar/Public/Test/OKM/TestInventoryWidget.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseWidget.h"
+#include "Components/TileView.h"
+#include "Components/Button.h"
+#include "GameSystem/Instance/Game/InventorySystem.h"
+#include "Data/Game/ItemData.h"
+#include "TestInventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UTestInventoryWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+
+public:
+	virtual void OpenUI() override;
+	virtual void CloseUI() override;
+
+	void FilterReset();
+	void FilterWeapon();
+	void FilterArmor();
+	void FilterHelmet();
+	void FIlterGrowth();
+	
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CloseButton = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> FilterResetButton = nullptr;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> FilterWeaponButton = nullptr;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> FilterArmorButton = nullptr;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> FilterHelmetButton = nullptr;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> FilterGrowthButton = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTileView> TileView = nullptr;
+
+private:
+	UInventorySystem* InventorySystem;
+
+	TArray<UItemData*> CachedItemList;
+};
