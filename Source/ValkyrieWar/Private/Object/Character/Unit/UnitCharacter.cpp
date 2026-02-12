9515118e0da4e4183b987efa3898acb7c110edb9
hyunwoo <allegoria88@gmail.com>
2026-02-12 14:11:47 +0900

    [성현우] 발키리 캐릭터 2종 세팅
    - 아처, 레이피어

--- Source/ValkyrieWar/Private/Object/Character/Unit/UnitCharacter.cpp
+++ Source/ValkyrieWar/Private/Object/Character/Unit/UnitCharacter.cpp
@@ -3,26 +3,6 @@
 
 #include "Object/Character/Unit/UnitCharacter.h"
 
-void AUnitCharacter::OnConstruction(const FTransform& Transform)
-{
-    Super::OnConstruction(Transform);
-
-    //하위 파츠 세팅
-    USkeletalMeshComponent* LeaderMesh = GetMesh();
-
-    TArray<USkeletalMeshComponent*> ModularParts;
-    GetComponents<USkeletalMeshComponent>(ModularParts);
-
-    for (USkeletalMeshComponent* Part : ModularParts)
-    {
-        if (Part == LeaderMesh) continue;
-
-        if (Part->ComponentHasTag(TEXT("Weapon"))) continue;
-
-        Part->SetLeaderPoseComponent(LeaderMesh);
-    }
-}
-
 void AUnitCharacter::PostInitializeComponents()
 {
     Super::PostInitializeComponents();
