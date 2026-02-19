e4dbd839ff47d9b4331c188a9f256ba88d186c7c
hyunwoo <allegoria88@gmail.com>
2026-02-19 13:54:50 +0900

    [성현우] 캐릭터 기본 장비 장착 및 장비 장착 처리 추가

--- Source/ValkyrieWar/Private/Data/Module/SkillModule.cpp
+++ Source/ValkyrieWar/Private/Data/Module/SkillModule.cpp
@@ -30,6 +30,21 @@ TArray<USkillData*> USkillModule::GetSkillData(TArray<int32> InDataId)
 	return DataList;
 }
 
+TArray<USkillData*> USkillModule::GetSkillData(TArray<int32> InDataId)
+{
+	TArray<USkillData*> DataList;
+
+	for (int32 DataId : InDataId)
+	{
+		if (SkillData.IsEmpty() || !SkillData.Contains(DataId))
+			continue;
+
+		DataList.Add(*SkillData.Find(DataId));
+	}
+	
+	return DataList;
+}
+
 void USkillModule::MakeData()
 {
 	if (DataTable)
