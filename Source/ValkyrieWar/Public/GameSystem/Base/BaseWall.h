2c6d77d19bd6997ea407b5232e092a0c0cbb8ed9
sdchjjj <sdchjjj@gmail.com>
2026-02-20 16:14:50 +0900

    [서동철] 기초 성벽 액터 추가
     - BaseWall 추가

---
+++ Source/ValkyrieWar/Public/GameSystem/Base/BaseWall.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseActor.h"
+#include "Data/Enum/CommonEnums.h"
+#include "BaseWall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API ABaseWall : public ABaseActor
+{
+	GENERATED_BODY()
+
+public:
+    ABaseWall();
+
+    UPROPERTY(EditAnywhere, Category = "Team")
+    ETeam Team = ETeam::TeamA;
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+};
