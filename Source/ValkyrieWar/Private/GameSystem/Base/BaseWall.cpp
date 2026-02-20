e197df7e78f4af19541884c54c108f83e2f936ea
sdchjjj <sdchjjj@gmail.com>
2026-02-20 16:14:50 +0900

    [서동철] 기초 성벽 액터 추가
     - BaseWall 추가

---
+++ Source/ValkyrieWar/Private/GameSystem/Base/BaseWall.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameSystem/Base/BaseWall.h"
+#include "GameSystem/Instance/World/BattleDirectorSubsystem.h"
+
+ABaseWall::ABaseWall()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABaseWall::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (UWorld* World = GetWorld())
+    {
+        if (UBattleDirectorSubsystem* Subsys = World->GetSubsystem<UBattleDirectorSubsystem>())
+        {
+            Subsys->RegisterWallAnchor(Team, this);
+        }
+    }
+}
+
+void ABaseWall::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    if (UWorld* World = GetWorld())
+    {
+        if (UBattleDirectorSubsystem* Subsys = World->GetSubsystem<UBattleDirectorSubsystem>())
+        {
+            Subsys->UnregisterWallAnchor(Team, this);
+        }
+    }
+
+    Super::EndPlay(EndPlayReason);
+}
