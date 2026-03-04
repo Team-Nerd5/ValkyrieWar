1531981951040aef754bac8d8244e42a6b8ca68c
edwardsoung <allegoria88@Gmail.com>
2026-03-05 00:28:07 +0900

    [성현우] 캐릭터 정보 위젯 코드 생성

---
+++ Source/ValkyrieWar/Private/Widget/Popup/CharacterInfo/CharacterInfoWidget.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Popup/CharacterInfo/CharacterInfoWidget.h"
+#include "GameSystem/Instance/Game/UIManager.h"
+
+#include "Widget/HUD/LobbyWidget.h"
+
+void UCharacterInfoWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+}
+
+void UCharacterInfoWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void UCharacterInfoWidget::OpenUI()
+{
+	UIType = EUIType::PopupCharacterInfo;
+
+	Super::OpenUI();
+}
+
+void UCharacterInfoWidget::CloseUI()
+{
+	//로비 켜줘야함
+	if (UUIManager* UIManager = GetGameInstance()->GetSubsystem<UUIManager>())
+	{
+		UIManager->OpenUI<ULobbyWidget>(EUIType::Lobby);
+	}
+	Super::CloseUI();
+}
---
+++ Source/ValkyrieWar/Public/Widget/Popup/CharacterInfo/CharacterInfoWidget.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseWidget.h"
+#include "CharacterInfoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UCharacterInfoWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+public:
+	virtual void OpenUI() override;
+	virtual void CloseUI() override;
+};
