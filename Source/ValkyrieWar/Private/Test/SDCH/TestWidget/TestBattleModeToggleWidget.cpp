2bce365a386d498f555a604a99a52a08e3c14af6
sdchjjj <sdchjjj@gmail.com>
2026-03-05 09:54:55 +0900

    [서동철] 개발/테스트용 모드 체인지 버튼 추가
    # Conflicts:
    #	Content/Widget/HUD/WBP_Battle.uasset

---
+++ Source/ValkyrieWar/Private/Test/SDCH/TestWidget/TestBattleModeToggleWidget.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test/SDCH/TestWidget/TestBattleModeToggleWidget.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Object/Character/Valkyrie/Controller/ValkyrieCharacterController.h"
+
+void UTestBattleModeToggleWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ToggleButton)
+	{
+		ToggleButton->OnClicked.AddDynamic(this, &UTestBattleModeToggleWidget::OnClickToggle);
+	}
+
+	//RefreshVisual();
+}
+
+void UTestBattleModeToggleWidget::NativeDestruct()
+{
+	if (ToggleButton)
+	{
+		ToggleButton->OnClicked.RemoveDynamic(this, &UTestBattleModeToggleWidget::OnClickToggle);
+	}
+
+	Super::NativeDestruct();
+}
+
+void UTestBattleModeToggleWidget::OnClickToggle()
+{
+	if (APlayerController* PC = GetOwningPlayer())
+	{
+		if (AValkyrieCharacterController* VPC = Cast<AValkyrieCharacterController>(PC))
+		{
+			VPC->ToggleControlMode();
+		}
+	}
+
+	//RefreshVisual();
+}
+
+//void UTestBattleModeToggleWidget::RefreshVisual()
+//{
+//	if (!IconImage) return;
+//
+//	if (APlayerController* PC = GetOwningPlayer())
+//	{
+//		if (AValkyrieCharacterController* VPC = Cast<AValkyrieCharacterController>(PC))
+//		{
+//			IconImage->SetOpacity(1.0f);
+//		}
+//	}
+//}
---
+++ Source/ValkyrieWar/Public/Test/SDCH/TestWidget/TestBattleModeToggleWidget.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseWidget.h"
+#include "TestBattleModeToggleWidget.generated.h"
+
+class UButton;
+class UImage;
+
+UCLASS()
+class VALKYRIEWAR_API UTestBattleModeToggleWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ToggleButton;
+
+	//UPROPERTY(meta = (BindWidget))
+	//TObjectPtr<UImage> IconImage;
+
+	UFUNCTION()
+	void OnClickToggle();
+
+	//void RefreshVisual();
+};
