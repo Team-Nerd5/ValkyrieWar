eaf243322f30bd5fc3f4280eaf9d8c9f8d313f4f
sdchjjj <sdchjjj@gmail.com>
2026-03-13 12:45:02 +0900

    [서동철] Battle HUD에 컨트롤 모드 전환 버튼 추가

---
+++ Source/ValkyrieWar/Private/Widget/Item/Battle/ModeChangeButtonWidget.cpp
@@ -0,0 +1,93 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Item/Battle/ModeChangeButtonWidget.h"
+
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Object/Character/Valkyrie/Controller/ValkyrieCharacterController.h"
+#include "GameSystem/Instance/World/WorldEventSystem.h"
+
+void UModeChangeButtonWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (Image_Knob)
+	{
+		// 디자이너에서도 상태가 보이도록
+		FWidgetTransform Transform = Image_Knob->RenderTransform;
+		Image_Knob->SetRenderTransform(Transform);
+	}
+}
+
+void UModeChangeButtonWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	BindEvents();
+}
+
+void UModeChangeButtonWidget::NativeDestruct()
+{
+	UnbindEvents();
+	Super::NativeDestruct();
+}
+
+void UModeChangeButtonWidget::BindEvents()
+{
+	if (ModeSwitch)
+	{
+		ModeSwitch->OnClicked.AddDynamic(this, &UModeChangeButtonWidget::HandleModeSwitchClicked);
+	}
+
+	if (UWorldEventSystem* WorldEventSystem = UGameBaseLibrary::GetWorldEventSystem(this))
+	{
+		WorldEventSystem->Battle.OnBattleModeChanged.AddDynamic(this, &UModeChangeButtonWidget::RefreshVisual);
+	}
+}
+
+void UModeChangeButtonWidget::UnbindEvents()
+{
+	if (ModeSwitch)
+	{
+		ModeSwitch->OnClicked.RemoveDynamic(this, &UModeChangeButtonWidget::HandleModeSwitchClicked);
+	}
+}
+
+void UModeChangeButtonWidget::HandleModeSwitchClicked()
+{
+	ToggleAutoMode();
+}
+
+void UModeChangeButtonWidget::ToggleAutoMode()
+{
+	if (APlayerController* PC = GetOwningPlayer())
+	{
+		if (AValkyrieCharacterController* VPC = Cast<AValkyrieCharacterController>(PC))
+		{
+			VPC->ToggleControlMode();
+		}
+	}
+}
+
+void UModeChangeButtonWidget::RefreshVisual(EInputControlMode InCurrentMode)
+{
+	bIsAutoMode = (InCurrentMode == EInputControlMode::Auto ? true : false);
+	ModeText->SetText(bIsAutoMode ? AutoText : ManualText);
+
+	if (Image_Knob)
+	{
+		// 애니메이션이 있으면 재생
+		if (Anim_ModeChange)
+		{
+			if (InCurrentMode == EInputControlMode::Auto)
+			{
+				PlayAnimationForward(Anim_ModeChange);
+			}
+			else
+			{
+				PlayAnimationReverse(Anim_ModeChange);
+			}
+		}
+	}
+}
---
+++ Source/ValkyrieWar/Public/Widget/Item/Battle/ModeChangeButtonWidget.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Data/Enum/CommonEnums.h"
+#include "ModeChangeButtonWidget.generated.h"
+
+class UButton;
+class UImage;
+class UWidgetAnimation;
+class UTextBlock;
+
+UCLASS()
+class VALKYRIEWAR_API UModeChangeButtonWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "ModeSwitch")
+	void ToggleAutoMode();
+
+	UFUNCTION(BlueprintPure, Category = "ModeSwitch")
+	bool IsAutoMode() const { return bIsAutoMode; }
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	virtual void NativePreConstruct() override;
+
+protected:
+	UFUNCTION()
+	void HandleModeSwitchClicked();
+
+	UFUNCTION()
+	void RefreshVisual(EInputControlMode InCurrentMode);
+	void BindEvents();
+	void UnbindEvents();
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ModeSwitch = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Background = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Knob = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> ModeText = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ModeSwitch|Style")
+	FText ManualText = FText::FromString(TEXT("Manual"));
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ModeSwitch|Style")
+	FText AutoText = FText::FromString(TEXT("Auto"));
+
+	// 노브 이동 애니메이션
+	UPROPERTY(Transient, meta = (BindWidgetAnimOptional))
+	TObjectPtr<UWidgetAnimation> Anim_ModeChange = nullptr;
+
+protected:
+	// 기본 상태
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ModeSwitch")
+	bool bIsAutoMode = false;
+};
