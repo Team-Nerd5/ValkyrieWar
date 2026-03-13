d2682f4a04885068432af89e55b433f9352385d0
sdchjjj <sdchjjj@gmail.com>
2026-03-13 15:17:08 +0900

    [서동철] Mana Ampul 추가

---
+++ Source/ValkyrieWar/Private/Widget/Item/Battle/ManaAmpulWidget.cpp
@@ -0,0 +1,100 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/Item/Battle/ManaAmpulWidget.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include <GameSystem/Instance/World/WorldEventSystem.h>
+
+void UManaAmpulWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (UWorldEventSystem* WorldEventSystem = UGameBaseLibrary::GetWorldEventSystem(this))
+	{
+		WorldEventSystem->Battle.OnCurrentManaChanged.AddDynamic(this, &UManaAmpulWidget::SetCurrentMana);
+		WorldEventSystem->Battle.OnBattleModeChanged.AddDynamic(this, &UManaAmpulWidget::OnControllModeChanged);
+	}
+}
+
+void UManaAmpulWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	CacheDynamicMaterial();
+	RefreshManaUI();
+}
+
+void UManaAmpulWidget::SetCurrentMana(int InCurrentMana, int InMaxMana)
+{
+	CurrentMana = FMath::Clamp((float)InCurrentMana, 0.f, FMath::Max(1.f, (float)InMaxMana));
+
+	ManaPercent = CurrentMana / (float)InMaxMana;
+
+	RefreshManaUI();
+}
+
+void UManaAmpulWidget::RefreshManaUI()
+{
+	if (!ManaFillMID)
+	{
+		CacheDynamicMaterial();
+	}
+
+	UpdateFillMaterial();
+
+	if (IsManaFull())
+	{
+		Image_Glow->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Image_Glow->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void UManaAmpulWidget::CacheDynamicMaterial()
+{
+	if (!Image_ManaFill)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ManaAmpulWidget] Image_ManaFill is null."));
+		return;
+	}
+
+	// Brush에 머티리얼이 설정되어 있으면 이를 MID로 가져온다
+	ManaFillMID = Image_ManaFill->GetDynamicMaterial();
+
+	if (!ManaFillMID)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("[ManaAmpulWidget] Failed to get dynamic material from Image_ManaFill."));
+	}
+}
+
+void UManaAmpulWidget::UpdateFillMaterial()
+{
+	if (!ManaFillMID)
+	{
+		return;
+	}
+
+	ManaFillMID->SetScalarParameterValue(FillPercentParameterName, ManaPercent);
+}
+
+void UManaAmpulWidget::OnControllModeChanged(EInputControlMode InMode)
+{
+	bIsAutoMode = (InMode == EInputControlMode::Auto) ? true : false;
+
+	if (Anim_ModeChange)
+	{
+		if (bIsAutoMode)
+		{
+			PlayAnimationForward(Anim_ModeChange);
+		}
+		else
+		{
+			PlayAnimationReverse(Anim_ModeChange);
+		}
+	}
+}
---
+++ Source/ValkyrieWar/Public/Widget/Item/Battle/ManaAmpulWidget.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Data/Enum/CommonEnums.h"
+#include "ManaAmpulWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+class UMaterialInstanceDynamic;
+
+/**
+ * 마나 앰플 UI 위젯
+ * - Image_ManaFill에 적용된 UI 머티리얼의 FillPercent를 갱신
+ * - 텍스트(Current / Max) 갱신
+ */
+UCLASS()
+class VALKYRIEWAR_API UManaAmpulWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+
+	/** 현재 마나만 변경 */
+	UFUNCTION(BlueprintCallable, Category = "ManaAmpul")
+	void SetCurrentMana(int InCurrentMana, int InMaxMana);
+
+	/** 외부에서 강제로 다시 그리기 */
+	UFUNCTION(BlueprintCallable, Category = "ManaAmpul")
+	void RefreshManaUI();
+
+protected:
+	void CacheDynamicMaterial();
+	void UpdateFillMaterial();
+
+private:
+	UFUNCTION()
+	void OnControllModeChanged(EInputControlMode InMode);
+
+	bool IsManaFull() { return ManaPercent >= 1.f; }
+
+protected:
+	/** Fill 머티리얼의 스칼라 파라미터명 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul")
+	FName FillPercentParameterName = TEXT("FillPercent");
+
+	/** 디자이너/초기값 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul")
+	float CurrentMana = 1000.f;
+
+	/** 너무 낮은 값일 때도 약간 보이게 하고 싶으면 사용 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul")
+	bool bUseMinimumVisualPercent = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul", meta = (EditCondition = "bUseMinimumVisualPercent", ClampMin = "0.0", ClampMax = "1.0"))
+	float MinimumVisualPercent = 0.05f;
+
+	/** 텍스트를 "현재 / 최대" 형식으로 표시할지 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul")
+	bool bShowMaxManaText = true;
+
+	/** 정수처럼 보이게 표시 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ManaAmpul")
+	bool bRoundTextValue = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "ModeSwitch")
+	bool bIsAutoMode = false;
+
+	/** 머티리얼이 적용된 Fill Image */
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_ManaFill = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_Glow = nullptr;
+
+	/** 런타임에 가져온 Fill용 MID */
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> ManaFillMID = nullptr;
+
+private:
+	UPROPERTY(Transient, meta = (BindWidgetAnimOptional))
+	TObjectPtr<UWidgetAnimation> Anim_ModeChange;
+
+	float ManaPercent = 0.f;
+};
