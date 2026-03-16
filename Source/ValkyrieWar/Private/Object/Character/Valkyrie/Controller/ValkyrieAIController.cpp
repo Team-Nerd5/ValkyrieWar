21aa073dc05ed73e5b12a3a7ac8a601959f18822
Dumbass <wjdtjdgus4548@gmail.com>
2026-03-16 15:09:13 +0900

    [정성현] 유령에셋정리, 폴더 정리

---
+++ Source/ValkyrieWar/Private/Object/Character/Valkyrie/Controller/ValkyrieAIController.cpp
@@ -0,0 +1,16 @@
+﻿#include "Object/Character/Valkyrie/Controller/ValkyrieAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+AValkyrieAIController::AValkyrieAIController()
+{
+	
+	
+}
+
+void AValkyrieAIController::OnPossess(APawn* InPawn)
+{
+}
