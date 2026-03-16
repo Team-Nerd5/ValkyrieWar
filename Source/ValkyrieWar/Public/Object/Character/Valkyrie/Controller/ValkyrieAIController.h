d1071d7b90d29aaa68a8ccf08b4b8160792a5df4
Dumbass <wjdtjdgus4548@gmail.com>
2026-03-16 17:00:17 +0900

    [정성현] 발키리 자동모드ai 구현중 상대방한테 움직이기 까지

--- Source/ValkyrieWar/Public/Object/Character/Valkyrie/Controller/ValkyrieAIController.h
+++ Source/ValkyrieWar/Public/Object/Character/Valkyrie/Controller/ValkyrieAIController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
 #include "ValkyrieAIController.generated.h"
 
 /**
@@ -17,14 +18,35 @@ class VALKYRIEWAR_API AValkyrieAIController : public AAIController
 public:
 	AValkyrieAIController();
 
-	virtual void  OnPossess(APawn* InPawn) override;
 protected:
-	UPROPERTY()
+	virtual void BeginPlay() override;
+	virtual void OnPossess(APawn* InPawn) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Sight")
+	float AISightRadius = 1000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Sight")
+	float AILoseSightRadius = 1200.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI|Sight")
+	float AISightAngle = 180.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	TObjectPtr<class UBlackboardComponent> BlackboardComp;
-	UPROPERTY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
 	TObjectPtr<class UBehaviorTreeComponent> BehaviorTreeComp;
 
 	//에디터에서 할당
 	UPROPERTY(EditAnywhere, Category = "AI")
 	TObjectPtr<class UBehaviorTree> BTAsset;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI") // 감각
+	TObjectPtr<class UAIPerceptionComponent> AIPerceptionComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	TObjectPtr<class UAISenseConfig_Sight> SightConfig;
+
+	UFUNCTION()
+	void OnTargetDetected(AActor* Actor, FAIStimulus const Stimulus);
 };
