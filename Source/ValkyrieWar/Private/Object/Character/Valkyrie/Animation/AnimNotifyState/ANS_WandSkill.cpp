8692c0c919d34381c5d541ecf73cadc1f37a631f
Dumbass <wjdtjdgus4548@gmail.com>
2026-03-18 15:53:49 +0900

    [정성현] 스킬몽타주에 사운드추가, 이펙트 추가

---
+++ Source/ValkyrieWar/Private/Object/Character/Valkyrie/Animation/AnimNotifyState/ANS_WandSkill.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/Character/Valkyrie/Animation/AnimNotifyState/ANS_WandSkill.h"
+#include "Object/Character/Valkyrie/ValkyrieCharacter.h"
+
+void UANS_WandSkill::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	
+}
+
+
+void UANS_WandSkill::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	
+}
