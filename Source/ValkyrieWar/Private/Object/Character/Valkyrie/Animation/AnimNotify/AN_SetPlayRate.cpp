6cae3787add660a28b5fe656d33793d36bebf0a8
Dumbass <wjdtjdgus4548@gmail.com>
2026-03-19 10:47:30 +0900

    [정성현] 몽타주 구간 재생속도 변경 노티파이 구현

---
+++ Source/ValkyrieWar/Private/Object/Character/Valkyrie/Animation/AnimNotify/AN_SetPlayRate.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Object/Character/Valkyrie/Animation/AnimNotify/AN_SetPlayRate.h"
+
+UAN_SetPlayRate::UAN_SetPlayRate()
+{
+	TargetPlayRate = 1.0f;
+}
+
+void UAN_SetPlayRate::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (MeshComp && MeshComp->GetAnimInstance())
+	{
+		UAnimInstance* AnimInstance = MeshComp->GetAnimInstance();
+		UAnimMontage* CurrentMontage = AnimInstance->GetCurrentActiveMontage();
+
+		if (CurrentMontage)
+		{
+			AnimInstance->Montage_SetPlayRate(CurrentMontage, TargetPlayRate);
+		}
+	}
+}
---
+++ Source/ValkyrieWar/Public/Object/Character/Valkyrie/Animation/AnimNotify/AN_SetPlayRate.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_SetPlayRate.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UAN_SetPlayRate : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UAN_SetPlayRate();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AnimNotify")
+	float TargetPlayRate;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
