318d808d3ddca747c948eed9ae587186ba5a1d6d
Dumbass <wjdtjdgus4548@gmail.com>
2026-03-19 17:56:52 +0900

    [정성현] 가챠 ui생성중

---
+++ Source/ValkyrieWar/Private/Widget/HUD/GachaWidget.cpp
@@ -0,0 +1,71 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/HUD/GachaWidget.h"
+
+#include "Components/Button.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+#include "GameSystem/Instance/Game/UIManager.h"
+
+void UGachaWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+    CreateTopMenu();
+    RefreshUI();
+
+    if (Btn_Summon_1x)
+    {
+        Btn_Summon_1x->OnClicked.AddDynamic(this, &UGachaWidget::OnClickSummon1x);
+    }
+    if (Btn_Summon_10x)
+    {
+        Btn_Summon_10x->OnClicked.AddDynamic(this, &UGachaWidget::OnClickSummon10x);
+    }
+}
+
+void UGachaWidget::NativeDestruct()
+{
+    Super::NativeDestruct();
+
+   
+    if (Btn_Summon_1x)
+    {
+        Btn_Summon_1x->OnClicked.RemoveDynamic(this, &UGachaWidget::OnClickSummon1x);
+    }
+    if (Btn_Summon_10x)
+    {
+        Btn_Summon_10x->OnClicked.RemoveDynamic(this, &UGachaWidget::OnClickSummon10x);
+    }
+}
+
+void UGachaWidget::OnClickSummon1x()
+{
+    // TODO : 가챠연출? & 서버에 패킷전송
+}
+
+void UGachaWidget::OnClickSummon10x()
+{
+    // TODO : 위에 내용과 똑같음 * 10
+}
+
+void UGachaWidget::RefreshUI()
+{
+    Super::RefreshUI();
+
+    int32 CurrentPity = 75;
+    int32 MaxPity = 100;
+
+    if (Text_PityCount)
+    {
+        FString PityStr = FString::Printf(TEXT("%d | %d"), CurrentPity, MaxPity);
+        Text_PityCount->SetText(FText::FromString(PityStr));
+    }
+    if (PB_Pity)
+    {
+        float PityPercent = (float)CurrentPity / (float)MaxPity;
+        PB_Pity->SetPercent(PityPercent);
+    }
+}
---
+++ Source/ValkyrieWar/Public/Widget/HUD/GachaWidget.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameSystem/Base/BaseWidget.h"
+#include "GachaWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALKYRIEWAR_API UGachaWidget : public UBaseWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UButton> Btn_Summon_1x;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UButton> Btn_Summon_10x;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UProgressBar> PB_Pity;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<class UTextBlock> Text_PityCount;
+protected:
+
+    virtual void NativeConstruct() override;
+    virtual void NativeDestruct() override;
+    virtual void RefreshUI() override;
+
+    UFUNCTION()
+    void OnClickSummon1x();
+
+    UFUNCTION()
+    void OnClickSummon10x();
+};
